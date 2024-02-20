bec0b56ee4f798482020b42cebf30398cd79b896
Alex Rivé <alex2rive3@gmail.com>
2024-02-20 16:51:33 -0300

    Delete decisiones/decisiones.template.js

--- decisiones/decisiones.template.js
+++
@@ -1,5 +0,0 @@
-//Escribe una función que reciba tres números y devuelva el mayor de los tres. *"debe retornar solo el numero"
-function cualEsMayor(a,b,c){
-    
-}
-module.exports = {cualEsMayor};   
